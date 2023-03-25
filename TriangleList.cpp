45253091a11199e27cbca2efad548bafb8b2af93
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2023-03-25 23:22:46 +0000

    Added TriangleList class

---
+++ TriangleList.cpp
@@ -0,0 +1,48 @@
+//
+//  TriangleList.cpp
+//  tm4
+//
+//  Created by anirudh-pulavarthy on 3/25/23.
+//
+
+#include <iostream>
+#include "TriangleList.hpp"
+
+bool TriangleList::alreadyAdded(Triangle t) {
+    if ( arrList.size() <= 1 )
+        return false;
+    
+    auto itr = std::find(arrList.begin(), arrList.end(), t);
+    
+    if ( itr == arrList.end() )
+        return false;
+    else if ( *itr == Triangle(0, 0, 0) )
+        return false;
+    
+    return true;
+}
+
+bool TriangleList::addTriangle(int vertices[3]) {
+    
+    if (vertices[0] == vertices[1]
+        || vertices[0] == vertices[2]
+        || vertices[1] == vertices[2])
+            return false;
+    
+    Triangle t(vertices[0], vertices[1], vertices[2]);
+    if (alreadyAdded(t))
+        return false;
+    
+    arrList.push_back(t);
+    return true;
+}
+
+Triangle TriangleList::get(int index) {
+    if (index < 0 || index >= arrList.size())
+    {
+        std::cout << "Triangle index out of bounds" << std::endl;
+        return Triangle(0, 0, 0);
+    }
+    
+    return arrList.at(index);
+}
---
+++ TriangleList.hpp
@@ -0,0 +1,28 @@
+//
+//  TriangleList.hpp
+//  tm4
+//
+//  Created by anirudh-pulavarthy on 3/25/23.
+//
+
+#ifndef TriangleList_hpp
+#define TriangleList_hpp
+
+#include <vector>
+#include "Triangle.hpp"
+
+class TriangleList {
+private:
+    std::vector<Triangle> arrList;
+    
+public:
+    bool addTriangle(int vertices[3]);
+    std::vector<Triangle>::iterator begin() { return arrList.begin(); }
+    std::vector<Triangle>::iterator end() { return arrList.end(); }
+    Triangle get(int index);
+    int size() { return (int) arrList.size(); }
+private:
+    bool alreadyAdded(Triangle t);
+};
+
+#endif /* TriangleList_hpp */
